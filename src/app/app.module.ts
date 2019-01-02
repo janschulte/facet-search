bf411b5f3e8c5257cf244f86f5fd78ed2f9ff68b
Jan Schulte <j.schulte@52north.org>
2019-01-02 13:07:21 +0100

    add helogland core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DatasetApiInterface, DatasetImplApiInterface, HelgolandCoreModule } from '@helgoland/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +12,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => new TranslateHttpLoader(http, './assets/i18n/', '.json'),
+        deps: [HttpClient]
+      }
+    }),
+    HelgolandCoreModule
   ],
-  providers: [],
+  providers: [
+    { useClass: DatasetImplApiInterface, provide: DatasetApiInterface }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
