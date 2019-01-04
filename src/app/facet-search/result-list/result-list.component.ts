130ac88c41f7feb2ee904d1226228af0fed32f7f
Jan Schulte <j.schulte@52north.org>
2019-01-04 11:53:40 +0100

    add list selection

--- src/app/facet-search/result-list/result-list.component.ts
+++ src/app/facet-search/result-list/result-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Timeseries } from '@helgoland/core';
 
 import { FacetSearch } from '../facet-search';
@@ -13,7 +13,10 @@ export class ResultListComponent implements OnInit {
   @Input()
   public facetSearchService: FacetSearch;
 
-  public results: Timeseries[];
+  @Output()
+  public selected: EventEmitter<Timeseries> = new EventEmitter();
+
+  public timeseries: Timeseries[];
 
   constructor() { }
 
@@ -21,8 +24,12 @@ export class ResultListComponent implements OnInit {
     if (!this.facetSearchService) {
       console.error('ResultListComponent needs a facet search service');
     }
-    this.facetSearchService.onResultsChanged.subscribe(ts => this.results = ts);
-    this.results = this.facetSearchService.getFilteredResults();
+    this.facetSearchService.onResultsChanged.subscribe(ts => this.timeseries = ts);
+    this.timeseries = this.facetSearchService.getFilteredResults();
+  }
+
+  public timeseriesSelected(ts: Timeseries) {
+    this.selected.emit(ts);
   }
 
 }
